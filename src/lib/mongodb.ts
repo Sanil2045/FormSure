6d905a6692c9abdcb45aa8fa838ab21dbc522064
Wellink <gamesaccountalex@yandex.ru>
2025-07-01 19:45:04 +0300

    project base

---
+++ src/lib/mongodb.ts
@@ -0,0 +1,26 @@
+// client/lib/mongodb.ts
+import mongoose from 'mongoose';
+
+const MONGODB_URI = process.env.MONGODB_URI!;
+
+declare global {
+  var mongooseCache: {
+    conn: typeof mongoose | null;
+    promise: Promise<typeof mongoose> | null;
+  };
+}
+
+global.mongooseCache = global.mongooseCache || { conn: null, promise: null };
+
+export async function connectDB() {
+  if (global.mongooseCache.conn) return global.mongooseCache.conn;
+
+  if (!global.mongooseCache.promise) {
+    global.mongooseCache.promise = mongoose.connect(MONGODB_URI, {
+      dbName: 'veriform', // опционально
+    });
+  }
+
+  global.mongooseCache.conn = await global.mongooseCache.promise;
+  return global.mongooseCache.conn;
+}
